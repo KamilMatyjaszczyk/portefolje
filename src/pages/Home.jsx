b505fdb47db10182e3c8dccf1fe746d4d8010ffd
KamilMatyjaszczyk <Kamilmaty@hotmail.com>
2025-02-02 21:58:10 +0100

    sky og glb filene

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-unknown-property */
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from "../models/Island.jsx";
+import Sky from "../models/Sky.jsx";
 
 
 const Home = () => {
@@ -28,11 +30,10 @@ const Home = () => {
                 className="w-full h-screen bg-transparent"
                 camera={{near :0.1, far:1000}}>
             <Suspense fallback={<Loader />}>
-                <directionalLight />
-                <ambientLight />
-                <pointLight />
-                <spotLight />
-                <hemisphereLight />
+                <directionalLight position={[1, 1, 1]} intensity={2} />
+                <ambientLight intensity={0.5} />
+                <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
+                <Sky />
                 <Island
                     position = {islandPosition}
                     scale = {islandScale}
