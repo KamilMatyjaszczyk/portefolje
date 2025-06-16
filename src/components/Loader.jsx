b57078a8591bcb3c57252be190330ca5c6e8f6d7
KamilMatyjaszczyk <Kamilmaty@hotmail.com>
2025-06-16 22:56:41 +0200

    Contact kommer en vei

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,14 @@
-import { Html } from '@react-three/drei'
+import { Html } from '@react-three/drei';
 
 const Loader = () => {
     return (
-        <Html>
-            <div className="flex justify-center items-center">
-                <div
-                    className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin">
-                </div>
-                Loader
+        <Html center>
+            <div className="flex flex-col justify-center items-center">
+                <div className="w-16 h-16 border-4 border-opacity-25 border-blue-500 border-t-blue-500 rounded-full animate-spin mb-4"></div>
+                <p className="text-white text-sm">Loading...</p>
             </div>
         </Html>
-    )
-}
+    );
+};
 
-export default Loader
+export default Loader;
